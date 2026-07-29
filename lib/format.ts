e7d194f0829ffeac9cb31515d50798213aa67986
nicejony <jonathanyeger@yahoo.com.ar>
2026-07-29 18:52:26 -0300

    Crear format.ts

---
+++ lib/format.ts
@@ -0,0 +1,6 @@
+export function formatearMoneda(valor: number): string {
+  return new Intl.NumberFormat("es-AR", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(valor);
+}
