f0280b1432d9162473756e28e591234a565014a0
nicejony <jonathanyeger@yahoo.com.ar>
2026-08-01 20:44:16 -0300

    Actualizar page.tsx

--- app/(panel)/productos/page.tsx
+++ app/(panel)/productos/page.tsx
@@ -1,6 +1,7 @@
 import { createClient } from "@/lib/supabase/server";
 import Link from "next/link";
 import BotonImprimir from "@/components/BotonImprimir";
+import { formatearMoneda } from "@/lib/format";
 
 export default async function ProductosPage() {
   const supabase = createClient();
