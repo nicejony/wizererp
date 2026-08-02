9910c01ae11e577e0c440c0ee732c963f6709f81
nicejony <jonathanyeger@yahoo.com.ar>
2026-08-02 19:27:21 -0300

    Actualizar CuentasCorrientesPanel.tsx

--- components/CuentasCorrientesPanel.tsx
+++ components/CuentasCorrientesPanel.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { createClient } from "@/lib/supabase/client";
+import { formatearMoneda } from "@/lib/format";
 
 interface Entidad {
   id: string;
